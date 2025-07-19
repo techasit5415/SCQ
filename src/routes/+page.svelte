c34994dbe0697ca73c71551f0c97b73445f28d91
techasit5415 <techasit2424@gmail.com>
2025-07-19 16:45:30 +0700

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,4 @@
-<!-- <h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
-<style>
-    p {
-        font-size: 25px;
-    }
-</style>
-
-<div style="text-align: center;">
-    <img src="/SCQ_logo.png" alt="SCQ Logo" width="200" height="200">
-    <p>Admin Login</p>
-    <input style="font-size:25px;" type="email" id="adminMail" name="adminMail"><br><br>
-    <input style="font-size:25px;" type="password" id="adminPwd" name="adminPwd"><br><br>
-    <input style="font-size:50px;" type="submit" value="Submit">
-</div>
-
+<h1>Hello form User</h1>
+<a href="/homeadmin">
+  <img src="https://media1.tenor.com/m/ue7Q8JmP_0MAAAAd/oiia-oiiaoiia.gif" alt="Admin GIF" style="width:200px;" />
+</a>
