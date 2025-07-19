278f840e19d144ab8c390b025b7335215e19d1c7
66050050_Kanin Mekkrajang <66050050@kmitl.ac.th>
2025-07-19 16:35:26 +0700

    Add email,password box

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
-<h1>Welcome to your library project</h1>
+<!-- <h1>Welcome to your library project</h1>
 <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p> -->
+<style>
+    p {
+        font-size: 25px;
+    }
+</style>
+
 <div style="text-align: center;">
-    <img src="/SCQ_logo.png" alt="SCQ Logo" width="400" height="400">
+    <img src="/SCQ_logo.png" alt="SCQ Logo" width="200" height="200">
     <p>Admin Login</p>
+    <input style="font-size:25px;" type="email" id="adminMail" name="adminMail"><br><br>
+    <input style="font-size:25px;" type="password" id="adminPwd" name="adminPwd"><br><br>
+    <input style="font-size:50px;" type="submit" value="Submit">
 </div>
+
