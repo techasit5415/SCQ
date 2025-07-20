25f0b8a6b2e2cfcfdf4bfc854f5f22a782ad689c
66050050_Kanin Mekkrajang <66050050@kmitl.ac.th>
2025-07-20 15:32:33 +0700

    add hidden menubar

--- src/routes/homeadmin/+page.svelte
+++ src/routes/homeadmin/+page.svelte
@@ -1,8 +1,23 @@
-<!-- <h1>hello world from admin</h1> -->
-<div class="menu-bar">
+<script>
+    function restaurantMenu() {
+        var x = document.getElementById("hiddenbar-container");
+        if (x.style.display === "none" || x.style.display === "") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+</script>
+
+<div id="menubar" class="menubar">
     <button type="button">Dashbord</button>
     <button type="button">Manage Users</button>
-    <button type="button">Manage Restaurant</button>
+    <button type="button" on:click={restaurantMenu}>Manage Restaurant</button>
+    <label id="hiddenbar-container">
+        <button type="button">Restaurant1</button>
+        <button type="button">Restaurant2</button>
+        <button type="button">Restaurant3</button>
+    </label>
     <button type="button">Reports</button>
     <button type="button">System Log</button>
     <button type="button">Setting</button>
@@ -33,20 +48,23 @@
         width: 100px;
         height: 100px;
     }
-    .menu-bar {
+    .menubar {
         height: 100%;
         width: 200px;
-        /* border-right: 1px solid #7f7f7f; */
         box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
         background: white;
         position: fixed;
         top: 10%;
+       
         left: 0;
+    }
+    label {
+        display: none;
     }
     button {
         width: 100%;
         padding: 1rem;
-        background-color: white;
+        background-color: rgb(255, 255, 255);
         border: none;
         font-size: 1rem;
         cursor: pointer;
