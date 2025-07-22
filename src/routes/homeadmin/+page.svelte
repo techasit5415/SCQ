d438d1f238435e64cf164e3ba699a130e608379e
techasit vanit <techasit2424@gmail.com>
2025-07-22 11:44:36 +0700

    update font and style in app.html and homeadmin page

--- src/routes/homeadmin/+page.svelte
+++ src/routes/homeadmin/+page.svelte
@@ -1,7 +1,7 @@
 <script>
     function restaurantMenu() {
         var x = document.getElementById("hiddenbar-container");
-        if(x) {
+        if (x) {
             if (x.style.display === "none" || x.style.display === "") {
                 x.style.display = "block";
             } else {
@@ -14,53 +14,63 @@
 <div id="menubar" class="menubar">
     <button type="button">
         <span class="material-symbols-outlined">dashboard</span>
-        Dashboard
+        <span class="btn-text">Dashboard</span>
     </button>
-    <button type="button"><span class="material-symbols-outlined">
-person
-</span>Manage Users</button>
-    <button type="button" on:click={restaurantMenu}><span class="material-symbols-outlined">
-storefront
-</span>Manage Restaurant</button>
+    <button type="button"
+        ><span class="material-symbols-outlined"> person </span><span class="btn-text">Manage Users</span></button
+    >
+    <button type="button" on:click={restaurantMenu}
+        ><span class="material-symbols-outlined"> storefront </span><span class="btn-text">Manage Restaurant</span></button
+    >
     <label id="hiddenbar-container">
-        <button type="button" class="restaurant">Restaurant1</button>
-        <button type="button" class="restaurant">Restaurant2</button>
-        <button type="button" class="restaurant">Restaurant3</button>
+        <button type="button" class="restaurant"><span class="btn-text">Restaurant2</span></button>
+        <button type="button" class="restaurant"><span class="btn-text">Restaurant2</span></button>
+        <button type="button" class="restaurant"><span class="btn-text">Restaurant3</span></button>
     </label>
-    <button type="button"><span class="material-symbols-outlined">
-assignment
-</span>Reports</button>
-    <button type="button"><span class="material-symbols-outlined">
-history
-</span>System Log</button>
-    <button type="button"><span class="material-symbols-outlined">
-settings
-</span>Setting</button>
+    <button type="button"
+        ><span class="material-symbols-outlined">
+            assignment
+        </span><span class="btn-text">Report</span></button
+    >
+    <button type="button"
+        ><span class="material-symbols-outlined"> history </span><span class="btn-text">System Log</span></button
+    >
+    <button type="button"
+        ><span class="material-symbols-outlined">
+            settings
+        </span><span class="btn-text">setting</span></button
+    >
 </div>
 
 <div class="header">
     <div class="photo">
-        <img src="/Photo/Icon.png" alt="icon">
+        <img src="/Photo/Icon.png" alt="icon" />
     </div>
     <div class="headtext">
-        <h1>SCQ</h1>
+        <h1>Admin panel</h1>
     </div>
 </div>
 
 <style>
     /* Icon  */
-.material-symbols-outlined {
-  font-variation-settings:
-  'FILL' 0,
-  'wght' 400,
-  'GRAD' 0,
-  'opsz' 24;
-  margin-right: 10px;
-}
-
+    .material-symbols-outlined {
+        font-variation-settings:
+            "FILL" 0,
+            "wght" 400,
+            "GRAD" 0,
+            "opsz" 24;
+        margin-right: 10px;
+        
+    }
+    .btn-text {
+        font-family: "Noto Sans Thai";
+        font-size: 16px;
+        font-weight: 400;
+        line-height: 19.2px;
+    }
     .header {
         display: flex;
-        height: 10%;
+        height: 60px;
         width: 100%;
         border-bottom: 2px solid rgb(221, 221, 221);
         background: white;
@@ -71,17 +81,17 @@ settings
     }
 
     .icon-menu {
-        width: 50px;
-        height: 50px;
+        width: 10px;
+        height: 10px;
     }
 
     .menubar {
-        height: 100%;
+        height: 120%;
         width: 250px;
         box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
         background: white;
         position: fixed;
-        top: 10%;
+        top: 8%;
         left: 0;
     }
 
@@ -97,7 +107,7 @@ settings
         display: flex;
         align-items: center;
         text-align: left;
-        height: 60px;
+        height: 40px;
         width: 100%;
         padding: 1rem;
         background-color: rgb(255, 255, 255);
@@ -109,6 +119,11 @@ settings
 
     h1 {
         color: #333;
+        font-family: "Noto Sans Thai";
+        font-size: 16px;
+        font-style: regular;
+        font-weight: 400;
+        line-height: 19.2px;
     }
 
     button:hover {
@@ -116,16 +131,15 @@ settings
     }
 
     .photo img {
-        width: 100px;
-        height: 100px;
+        width: 48px;
+        height: auto;
     }
 
-.material-symbols-outlined {
-  font-variation-settings:
-  'FILL' 0,
-  'wght' 400,
-  'GRAD' 0,
-  'opsz' 24
-}
-
+    .material-symbols-outlined {
+        font-variation-settings:
+            "FILL" 0,
+            "wght" 400,
+            "GRAD" 0,
+            "opsz" 24;
+    }
 </style>
