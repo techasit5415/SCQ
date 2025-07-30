8094d269498b1442f9040c7fa86e3766bfbc1ca2
66050050_Kanin Mekkrajang <66050050@kmitl.ac.th>
2025-07-30 22:56:37 +0700

    just commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.scq.svelteSCQ',
+  appName: 'SCQ App',
+  webDir: 'build'
+};
+
+export default config;
