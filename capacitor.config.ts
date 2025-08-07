565b40acf712cab762fee19b1ad428479e6410ca
66050050_Kanin Mekkrajang <66050050@kmitl.ac.th>
2025-08-07 14:26:12 +0700

    just commit

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.SciCanteenQueue.app',
-  appName: 'SCQ',
-  webDir: 'build'
-};
-
-export default config;
