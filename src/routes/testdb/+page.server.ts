1b506354bfe23678d719a862196b5b7aee838780
techasit vanit <techasit2424@gmail.com>
2025-09-24 03:24:46 +0700

    clean

--- src/routes/testdb/+page.server.ts
+++
@@ -1,7 +0,0 @@
-import { query } from '$lib/server/db.js';
-
-export async function load() {
-  const rows = await query('SELECT NOW() AS now');
-  console.log('DB Test:', rows);
-  return { now: rows[0].now };
-}
--- src/routes/testdb/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  export let data;
-</script>
-
-<h1>DB Test</h1>
-<p>Server Time: {data.now}</p>
